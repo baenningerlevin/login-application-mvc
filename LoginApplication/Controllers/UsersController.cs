32c71b79f1c82a3f8ac81d06e58573c83b5f4a8c
Levin Bänninger <l.baenninger@icloud.com>
2024-03-01 22:56:28 +0100

    Add project files.

---
+++ LoginApplication/Controllers/UsersController.cs
@@ -0,0 +1,71 @@
+﻿using LoginApplication.Data;
+using LoginApplication.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LoginApplication.Controllers
+{
+	public class UsersController : Controller
+	{
+		private ApplicationDbContext _db;
+		public UsersController(ApplicationDbContext db)
+		{
+			_db = db;
+		}
+
+		public IActionResult Index()
+		{
+			Login login = Login.GetInstance();
+
+			if (login != null)
+			{
+				return View();
+			}
+			return RedirectToAction("Index", "Login");
+		}
+
+		public IActionResult LogOut()
+		{
+			Login login = Login.GetInstance();
+			login.IsLoggedIn = false;
+			Login.LogOut();
+
+			_db.Update(login);
+			_db.SaveChanges();
+			return RedirectToAction("Index", "Login");
+		}
+
+		public IActionResult Register()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		public IActionResult Register(User user, string confirmPassword)
+		{
+			User existingUser = _db.Users.FirstOrDefault(u => u.Username == user.Username);
+			if (existingUser != null)
+			{
+				ModelState.AddModelError("Username", "Username already exists");
+			}
+
+			if (user.Password != confirmPassword)
+			{
+				ModelState.AddModelError("", "Passwords do not match");
+			}
+
+			if (ModelState.IsValid)
+			{
+				user.IsAdmin = false;
+				user.IsLoggedIn = false;
+
+				_db.Users.Add(user);
+				_db.SaveChanges();
+				return RedirectToAction("Index");
+			}
+			else
+			{
+				return View();
+			}
+		}
+	}
+}
---
+++ LoginApplication/Models/Login.cs
@@ -0,0 +1,33 @@
+﻿namespace LoginApplication.Models
+{
+	public class Login : User
+	{
+		private static Login? _instance = null;
+
+		public static Login GetInstance()
+		{
+			return _instance;
+		}
+
+		public static Login LogIn(User user)
+		{
+			if (_instance == null)
+			{
+				_instance = new Login();
+			}
+
+			_instance.Id = user.Id;
+			_instance.Username = user.Username;
+			_instance.Password = user.Password;
+			_instance.IsAdmin = user.IsAdmin;
+			_instance.IsLoggedIn = true;
+
+			return _instance;
+		}
+
+		public static void LogOut()
+		{
+			_instance = null;
+		}
+	}
+}
